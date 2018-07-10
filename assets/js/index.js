ad640dcf96631be66327d26f2a3bf962b8d5e3e5
Jose D. Alvarez <jdalvarez23@gmail.com>
2018-07-09 21:36:48 -0500

    Updated 07/09/2018 - Updated register client side, fixed html bugs, updated index.js file to comply with SPA techniques

--- assets/js/index.js
+++ assets/js/index.js
@@ -28,7 +28,7 @@ function initializeDatabase() {
 
 // Fetch Data Function (Retrieve user information from indexedDB)
 function fetchData() {
-  db.on('ready', function() { // Execute when indexedDB is on "ready" state
+  db.on('ready', function() { // execute when indexedDB is on "ready" state
     console.log("Fetching data...");
     return db.users.count(function(count) { // return information from indexedDB
       // Execute if there are users found in indexedDB
@@ -51,6 +51,58 @@ function fetchData() {
   });
 }
 
+// Write Data Function (Write user information to indexedDB)
+function writeData(type, data) {
+  console.log("...Writing data...");
+  console.log("Type: ", type);
+  console.log(data);
+
+  // Execute if user is entering data for first time
+  if (type == "firstTime") {
+    console.log("First time submitting data.");
+
+    db.transaction('rw', db.users, function () {
+      db.users.add(data); // write data to user's table
+    }).catch(function(err) { // execute if error occurs
+      console.error(err.stack || err);
+    });
+
+  // Execute if user is updating existing data
+  } else if (type == "update") {
+    db.users.update(1, {name: data['name'], location: data['location']}).then(function(updated) {
+      console.log(updated);
+      // Execute if data returned exists
+      if (updated) {
+        console.log ("Update successful!");
+        // Alert the user that data was successfully updated
+        bootbox.alert({
+          title: "Success",
+          message: "Successfully updated information!",
+          callback: function() {
+            console.log('Fetching data again...');
+          }
+        });
+      // Execute if data returned is null
+      } else if (updated == 0) {
+        console.log("Nothing to update!");
+        console.log('Fetching data again...');
+      // Execute if no data is returned
+      } else {
+        console.log ("Failed to update!");
+        // Alert the user of error
+        bootbox.alert({
+          title: "Error",
+          message: "There seems to be an error updating your information, please try again later!",
+          callback: function() {
+            console.log('Fetching data again...');
+          }
+        });
+      }
+    });
+    fetchData(); // method called to fetch user information from indexedDB
+  }
+}
+
 // Display Page Function (Load the page to the main content element using jQuery load method)
 function displayPage(page) {
   $("#main_Content").load("./page-content/loading.html", function() {
@@ -60,4 +112,10 @@ function displayPage(page) {
       $("#main_Content").load(pageURL);
     }, 1000);
   });
+}
+
+// Display Error Notification (Display an error div with error message)
+function displayError(id, message) {
+  $(id).find("#alert_Description").html(message);
+  $(id).removeClass('hide');
 }
